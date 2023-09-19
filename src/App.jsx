7c62d92f54532366892c0de205f47e523ee4c7c9
Jose Albornoz <prueba@outlook.cl>
2023-09-19 18:35:55 -0300

    Componente y estilos del Nabvar agregados

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./componentes/Navbar/Navbar";
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Navbar/>
+      </BrowserRouter>
     </>
   )
 }
