82aeb339de2a495b4c1dda6c7a29b7cf1b848968
Jose Albornoz <prueba@outlook.cl>
2023-09-27 00:16:20 -0300

    Se agregar una vista NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Provider } from './Context';
 import Navbar from './componentes/Navbar';
 import Home from './views/Home';
 import Favoritos from './views/Favoritos';
+import NotFound from './views/NotFound';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Routes>
             <Route path="/desafio2-naturalpic/" element={<Home />} />
             <Route path="/desafio2-naturalpic/favoritos" element={<Favoritos />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </Provider>
